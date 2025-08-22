8d480eec9269bf4b0e71ab3aef7be582ad25a6ef
Amogh Diwan <41579921+Fletchersan@users.noreply.github.com>
2025-08-22 23:16:57 +0530

    feat: fix errors, custom fdSet handling functions

--- 0-smoke-test/less-packages/echo_tcp.go
+++ 0-smoke-test/less-packages/echo_tcp.go
@@ -13,9 +13,9 @@ func server(port int) {
 	if err != nil {
 		log.Fatal("Socket: ", err)
 	}
-	listen_on := [4]byte{127, 0, 0, 1}
+	listen_on := [4]byte{0, 0, 0, 0}
 	socketAddr := unix.SockaddrInet4{
-		Port: 8000,
+		Port: port,
 		Addr: listen_on,
 	}
 
@@ -27,49 +27,89 @@ func server(port int) {
 	if err != nil {
 		log.Fatal("Listen: ", err)
 	}
+	log.Printf("Listening on port %d", port)
 	var activeFDSet, readFDSet unix.FdSet
-	activeFDSet.Bits = [32]int32{}
-	activeFDSet.Bits[socketFD/32] |= (1 << uint(socketFD) % 32)
+	FDZero(&activeFDSet)
+	FDSet(socketFD, &activeFDSet)
 	fdAddrMap := make(map[int]unix.Sockaddr, unix.FD_SETSIZE)
 	for {
 		readFDSet = activeFDSet
-		_, err := unix.Select(int(1024), &readFDSet, nil, nil, nil)
+		_, err := unix.Select(unix.FD_SETSIZE, &readFDSet, nil, nil, nil)
 		if err != nil {
 			log.Fatal("Select: ", err)
 		}
-		for i := 0; i < unix.FD_SETSIZE+1; i++ {
-			if readFDSet.Bits[i/32]&(1<<uint(i)%32) != 0 {
+		for i := 0; i < unix.FD_SETSIZE; i++ {
+			if isPosSet(i, &readFDSet) {
 				if i == socketFD {
 					newConn, newConnAddr, err := unix.Accept(socketFD)
 					if err != nil {
 						log.Fatal("Accept: ", err)
 					}
-					activeFDSet.Bits[newConn/32] |= (1 << uint(i) % 32)
+					FDSet(newConn, &activeFDSet)
 					fdAddrMap[newConn] = newConnAddr
+				} else {
+					msg := make([]byte, 4096)
+					msgSize, err := unix.Read(i, msg)
+					if err != nil {
+						log.Print("Recvfrom: ", err)
+						FDClr(i, &activeFDSet)
+						delete(fdAddrMap, i)
+						unix.Close(i)
+						continue
+					}
+					_, err = unix.Write(i, msg[:msgSize])
+					if err != nil {
+						log.Print("Sendmsg: ", err)
+					}
+					if msgSize == 0 {
+						FDClr(i, &activeFDSet)
+						delete(fdAddrMap, i)
+						unix.Close(i)
+					}
 				}
-			} else {
-				msg := make([]byte, 8000)
-				_, _, err := unix.Recvfrom(i, msg, 0)
-				if err != nil {
-					log.Print("Recvfrom: ", err)
-					activeFDSet.Bits[i/32] &^= (1 << (uint(i) % 32))
-					unix.Close(i)
-					delete(fdAddrMap, i)
-					continue
-				}
-				clientAddr := fdAddrMap[i].(*unix.SockaddrInet4)
-				err = unix.Sendmsg(
-					i, msg, nil, clientAddr, unix.MSG_DONTWAIT,
-				)
-				if err != nil {
-					log.Print("Sendmsg: ", err)
-				}
-				activeFDSet.Bits[i/32] &^= (1 << (uint(i) % 32))
-				delete(fdAddrMap, i)
-				unix.Close(i)
 			}
 		}
 	}
+}
+
+func FDZero(fdSet *unix.FdSet) {
+	for i := 0; i < len(fdSet.Bits); i++ {
+		fdSet.Bits[i] = 0
+	}
+}
+
+func FDSet(pos int, fdSet *unix.FdSet) {
+	if pos < 0 || pos >= unix.FD_SETSIZE {
+		return
+	}
+	index := pos / 64
+	bit := pos % 64
+	if index < len(fdSet.Bits) {
+		fdSet.Bits[index] |= (1 << bit)
+	}
+}
+
+func FDClr(pos int, fdSet *unix.FdSet) {
+	if pos < 0 || pos >= unix.FD_SETSIZE {
+		return
+	}
+	index := pos / 64
+	bit := pos % 64
+	if index < len(fdSet.Bits) {
+		fdSet.Bits[index] &^= (1 << bit)
+	}
+}
+
+func isPosSet(pos int, fdSet *unix.FdSet) bool {
+	if pos < 0 || pos >= unix.FD_SETSIZE {
+		return false
+	}
+	index := pos / 64
+	bit := pos % 64
+	if index < len(fdSet.Bits) {
+		return fdSet.Bits[index]&(1<<bit) != 0
+	}
+	return false
 }
 
 func main() {
